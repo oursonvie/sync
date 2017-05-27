dc694572425abe48e991f37b923118f05d1e9bd4
Tonny <oursonvie@qq.com>
2017-05-27 10:20:00 +0800

    incremental update

--- server/restJSON.js
+++ server/restJSON.js
@@ -41,3 +41,10 @@ JsonRoutes.add('get', '/login', function(req, res, next) {
     });
 
 });
+
+// program API
+JsonRoutes.add('get', '/userInfo', function(req, res, next) {
+  console.log(req.query);
+  console.log(req.query.userInfo);
+
+});
