2712489363bb9badb42f8d022fbb7088a253993d
Tonny <oursonvie@qq.com>
2017-05-27 10:00:50 +0800

    incremental update

--- server/restJSON.js
+++ server/restJSON.js
@@ -13,9 +13,28 @@ JsonRoutes.add('get', '/api', function(req, res, next) {
 JsonRoutes.add('get', '/login', function(req, res, next) {
     console.log(req.query.res);
 
+    var wxRES = req.query.res
 
+    var jscode = wxRES['code']
+    console.log(jscode)
+    var appid = 'wx49c58d3d75baabf1'
+    var secret = '49abff6b3020dd657a45534ca2f99e9c'
 
-    JsonRoutes.sendResult(res, {
-        text: 'hello world'
+    var url = 'https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code'
+
+    HTTP.call( 'GET', 'https://api.weixin.qq.com/sns/jscode2session?=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code', {
+      params: {
+        "appid" : appid,
+        "secret" : secret,
+        "js_code" : jscode,
+        "grant_type" : "authorization_code"
+      }
+    }, function( error, response ) {
+      if ( error ) {
+        console.log( error );
+      } else {
+        console.log( response );
+      }
     });
+
 });
